4497709d99cc71d77a3e7af23f6ebc7d26071518
Luke Goodridge <lukegoodridge@icloud.com>
2022-03-01 23:08:13 +0000

    New class added to track the users money

---
+++ TrainTicketSystem/wallet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainTicketSystem
+{
+    internal class Wallet
+    {
+        private float WalletTotal { get; set; }
+
+        // Let them have 50 in the wallet to begin with
+        public Wallet()
+        {
+            WalletTotal = 50.00f;
+        }
+        public bool DeductPrice(float price)
+        {
+            if (WalletTotal - price < 0)
+            {
+                return false;
+            }
+            else
+            {
+                WalletTotal -= price;
+                return true;
+            }
+        }
+    }
+}
